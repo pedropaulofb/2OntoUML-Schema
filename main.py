d76c3da4f4bcd97d5e0d811d3bd7dd15c7df8b41
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-05-12 11:30:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+""" Main file for loading XMI to OntoUML-Schema. """
+
+SOFTWARE_ACRONYM = "2OntoUML-Schema"
+SOFTWARE_NAME = "2OntoUML-Schema"
+SOFTWARE_VERSION = "23.05.12"
+SOFTWARE_URL = "https://github.com/OntoUML/ontouml-models-tools"
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
